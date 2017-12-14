ba1770e92727d693125d857fe00e21c8d4daa15a
charapod <podimata@g.harvard.edu>
2017-12-14 16:38:10 -0500

    Module for drawing an arm

---
+++ probability.py
@@ -0,0 +1,16 @@
+# Code for drawing an arm given the probabilities of the arms
+# Gets as input the list of the probabilities for each arm
+# Return the arm to be drawn
+import random
+
+def draw(probs_lst):
+    t           = random.random()
+    print ("t chosen:%f"%t)
+    cumulative  = 0.0
+    for i in range(0,len(probs_lst)):
+        cumulative += probs_lst[i]
+        if cumulative > t:
+            return i
+    return 0
+
+
