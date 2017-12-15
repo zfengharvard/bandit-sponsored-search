45e4b056fc42767ee7f554669ab8c1ca5af23adc
charapod <podimata@g.harvard.edu>
2017-12-15 11:57:49 -0500

    Regret module for an instance of T rounds

--- regret.py
+++ regret.py
@@ -1,22 +1,18 @@
 ############ 
-# Implements the regret both in the discretized case and
-# in the continuous
+# Implements the regret both in the discretized case
 ############ 
 
 import numpy as np
 
-def indicator(bid,price):
-    if bid >= price:
-        return 1
-    else:
-        return 0
-
-# Reward List is the list of the utilities that were realized
-# Reward List is a Txbid_space matrix
-# The allocation function is also a Txbid_space matrix
-# Because the regret that we compute is expected regret
 # TODO we need to run each instance of T rounds *multiple times* and take the average
-def regret(cont, v_lst, p_lst, alloc, eps, bid_space):
+# Arguments: 
+# - cont: 0 when discrete, 1 when continuous
+# - v_lst: 2D list with the realized valuations (size: num_rounds x bid_space)
+# - p_lst: 2D list with the realized payments (size: num_rounds x bid_space)
+# - alloc: 2D list with the realized allocation (size: num_rounds x bid_space)
+# - bid_space: number of arms
+# - algo_util: list with the utility that our algorithm has acquired (size: T) 
+def regret(cont, v_lst, p_lst, alloc, bid_space, algo_util):
     if cont == 0: #discrete case
         util = []
         for i in range(0,bid_space-1):
@@ -25,4 +21,4 @@ def regret(cont, v_lst, p_lst, alloc, eps, bid_space):
         max_util_hindsight = np.max(util)
     else: #continuous case
         # for now, we're not comparing with the continuous case
-    return max_util_hindsight
+    return (max_util_hindsight - sum(algo_util))
