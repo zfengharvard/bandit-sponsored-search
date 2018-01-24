2984da83d907337e4de44e78acab639595002a7f
charapod <podimata@g.harvard.edu>
2018-01-24 00:51:50 -0500

    included more print logs

--- master_file.py
+++ master_file.py
@@ -13,7 +13,6 @@ def regret_winexp(bidder, T,num_repetitions, num_bidders, num_slots, outcome_spa
         winexp_regr.append(main_winexp(bidder,rep, T, num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values))
 
         # After each repetition, re-initialize the weights and the probabilities of the arm
-
         for i in range(0, num_bidders):
             bidder[i].pi         = [bidder[i].eps for j in range(0,bidder[i].bid_space)]
             bidder[i].weights    = [1 for j in range(0,bidder[i].bid_space)]
@@ -34,7 +33,6 @@ def regret_exp3(bidder, T,num_repetitions, num_bidders, num_slots, outcome_space
         print ("Repetition rep %d for exp3"%rep)
         exp3_regr.append(main_exp3(bidder,rep, T, num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values))
         # After each repetition, re-initialize the weights and the probabilities of the arm
-
         for i in range(0, num_bidders):
             bidder[i].pi         = [bidder[i].eps for j in range(0,bidder[i].bid_space)]
             bidder[i].weights    = [1 for j in range(0,bidder[i].bid_space)]
@@ -42,6 +40,7 @@ def regret_exp3(bidder, T,num_repetitions, num_bidders, num_slots, outcome_space
             bidder[i].alloc_func = [[] for t in range(0,T)]
             bidder[i].pay_func   = [[] for t in range(0,T)]
             bidder[i].reward_func = [[] for t in range(0,T)]
+            bidder[i].loss       = [0 for j in range(0,bidder[i].bid_space)]
 
     exp3_expected_regr = sum(exp3_regr)/num_repetitions
 
