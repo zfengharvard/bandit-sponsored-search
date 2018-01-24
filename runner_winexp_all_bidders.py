8c06dd62fbbb2fd26d11fcf092adf2d756738ee7
charapod <podimata@g.harvard.edu>
2018-01-24 00:53:11 -0500

    copied the bids before pay_func called

--- runner_winexp_all_bidders.py
+++ runner_winexp_all_bidders.py
@@ -18,6 +18,8 @@ def compute_reward(allocation_func, payment_func, ctr, values):
     reward_lst = []
     for b in allocation_func:
         if b in ctr:
+            # If bidder isn't allocated any slot: b == 0
+            # However, b == 0 also if the CTR of the position is 0
             reward_lst.append(values[ctr.index(b)] - payment_func[allocation_func.index(b)])
         else :
             reward_lst.append(-1)
@@ -27,11 +29,11 @@ def compute_reward(allocation_func, payment_func, ctr, values):
 def main_winexp(bidder,curr_rep, T,num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values):
     bids = []
     for t in range(0,T):
-        #bids = [round(bidder[i].bidding(),2) for i in range(0,num_bidders)]
+        bids.append([round(bidder[i].bidding(),2) for i in range(0,num_bidders)])
         print ("Rank Scores at timestep %d"%t)
         print rank_scores[t]
         print ("Bids at timestep %d"%t)
-        bids.append([0.6, 0.0, 0.2, 0.1, 0.4])
+        #bids.append([0.6, 0.0, 0.2, 0.1, 0.4])
         print bids[t] 
         print ("CTR at timestep %d"%t)
         print ctr[t]
@@ -47,7 +49,8 @@ def main_winexp(bidder,curr_rep, T,num_bidders, num_slots, outcome_space, rank_s
             print ("Allocation Function for Bidder %d"%bidder[i].id)
             print bidder[i].alloc_func[t]
             #reward function: value - payment(coming from GSP module)
-            bidder[i].pay_func[t] = [GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).pay_func(bidder[i].id, bid*bidder[i].eps) for bid in range(0, bidder[i].bid_space)]  
+            bid_vec = deepcopy(bids[t])
+            bidder[i].pay_func[t] = [GSP(ctr[t], reserve[t], bid_vec, rank_scores[t], num_slots, num_bidders).pay_func(bidder[i].id, bid*bidder[i].eps) for bid in range(0, bidder[i].bid_space)]  
             print ("Payment Function for Bidder %d"%bidder[i].id)
             print bidder[i].pay_func[t]
             bidder[i].reward_func[t] = compute_reward(bidder[i].alloc_func[t], bidder[i].pay_func[t], ctr[t], values[t])
@@ -61,14 +64,21 @@ def main_winexp(bidder,curr_rep, T,num_bidders, num_slots, outcome_space, rank_s
                 bidder[i].utility[t] = (bidder[i].compute_utility(0, bidder[i].reward_func[t], bidder[i].alloc_func[t]))
             bidder[i].utility[t] = [0 if bidder[i].utility[t][bb] == -0.0 else bidder[i].utility[t][bb] for bb in range(0,bidder[i].bid_space)]
             #weights update
-            print ("Bidder's utility")
+            print ("Bidder %d utility"%i)
             print bidder[i].utility[t]
+            print ("Bidder %d weights and probabilities before the update at timestep %d"%(i,t))
+            print bidder[i].weights
+            print bidder[i].pi 
             bidder[i].weights_update_winexp(bidder[i].eta_winexp, bidder[i].utility[t])        
+            print ("Bidder %d weights and probabilities after the update at timestep %d"%(i,t))
+            print bidder[i].weights
+            print bidder[i].pi 
         
         
     # after the end of T timesteps, compute regrets for both the WIN-EXP algo
     for i in range(0, num_bidders):
-        algo_util = [bidder[i].utility[t][int(bids[t][bidder[i].id]/bidder[i].eps)] for t in range(0,T)]
+        arm_chosen = [int(math.ceil(bids[t][bidder[i].id]/bidder[i].eps)) for t in range(0,T)]  
+        algo_util = [(bidder[i].reward_func[t][arm_chosen[t]]*bidder[i].alloc_func[t][arm_chosen[t]] - 1) for t in range(0,T)]
         bidder[i].winexp_regret[curr_rep] = regret(0, bidder[i].reward_func, bidder[i].alloc_func, bidder[i].bid_space, algo_util, T)
     s = 0
     for i in range(0, num_bidders):
