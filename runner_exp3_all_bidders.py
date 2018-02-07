05360f59eb85d47122406f2443b288802bdc771d
charapod <podimata@g.harvard.edu>
2018-02-06 19:23:51 -0500

    has errors

--- runner_exp3_all_bidders.py
+++ runner_exp3_all_bidders.py
@@ -13,91 +13,79 @@ from regret import *
 from runner_winexp_all_bidders import compute_reward
 
 
-def main_exp3(bidder,curr_rep, T,num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values,bids):
-    
+def main_exp3(bidder,curr_rep, T,num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values,bids,num_auctions):
+    algo_util  = []
+    temp_regr  = []
     for t in range(0,T):
-        # bids[t] = []
-        bids[t][0] = round(bidder.bidding(),2)
-        print ("Bids inside exp3 at timestep %d"%t)
-        print bids[t]
-        #every bidder is a learner and they all should update the estimated utility that they get
-        allocated = GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder.id, bids[t][bidder.id])
-        bidder.alloc_func[t] = [GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]
-        #reward function: value - payment(coming from GSP module)
-        bid_vec = deepcopy(bids[t])
-        bidder.pay_func[t] = [GSP(ctr[t], reserve[t], bid_vec, rank_scores[t], num_slots, num_bidders).pay_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]  
+        bid_chosen = round(bidder.bidding(),2)
+        #print ("Bid chosen for timestep t=%d is:%f"%(t,bid_chosen))
+        for auction in range(0,num_auctions):
+            bids[auction][t][0] = bid_chosen
+            #print ("Repetition=%d, timestep t=%d, auction=%d"%(curr_rep, t,auction))
+            #print ("Rank scores")
+            #print rank_scores[auction][t]
+            #print ("CTR")
+            #print ctr[auction][t]
+            #print ("reserve")
+            #print reserve[auction][t]
+            #print ("bids")
+            #print bids[auction][t]
+            #every bidder is a learner and they all should update the estimated utility that they get
+            allocated = GSP(ctr[auction][t], reserve[auction][t], bids[auction][t], rank_scores[auction][t], num_slots, num_bidders).alloc_func(bidder.id, bids[auction][t][bidder.id])
+            bidder.alloc_func[auction][t] = [GSP(ctr[auction][t], reserve[auction][t], bids[auction][t], rank_scores[auction][t], num_slots, num_bidders).alloc_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]
+            #print ("Auction=%d, alloc func"%auction)
+            #print bidder.alloc_func
+            #reward function: value - payment(coming from GSP module)
+            bid_vec = deepcopy(bids[auction][t])
+            bidder.pay_func[t] = [GSP(ctr[auction][t], reserve[auction][t], bid_vec, rank_scores[auction][t], num_slots, num_bidders).pay_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]  
+            #print ("Auction=%d, pay func"%auction)
+            #print bidder.pay_func
     
-        bidder.reward_func[t] = compute_reward(bidder.alloc_func[t], bidder.pay_func[t], ctr[t], values[t])
-        #### EXP3 computations ####
-        bidder.utility[t] = [bidder.reward_func[t][b]*bidder.alloc_func[t][b] - 1 for b in range(0, bidder.bid_space)]
-        print ("Bidder %d utility"%bidder.id)
-        print bidder.utility[t]
+            bidder.reward_func[auction][t] = compute_reward(bidder.alloc_func[auction][t], bidder.pay_func[t], ctr[auction][t], values[auction][t],num_auctions)
+            #print ("Bidder's Reward function")
+            #print bidder.reward_func
+            #### EXP3 computations ####
+            #bidder.utility[auction][t] = [bidder.reward_func[auction][t][b]*bidder.alloc_func[auction][t][b] - 1 for b in range(0, bidder.bid_space)]
+            bidder.utility[auction][t] = [bidder.reward_func[auction][t][b]*bidder.alloc_func[auction][t][b] for b in range(0, bidder.bid_space)]
+            #print ("Bidder %d utility"%bidder.id)
+            #print bidder.utility
 
         #weights update
-        print bids[t], bids[t][bidder.id]
-        arm_chosen = int(math.ceil(bids[t][bidder.id]/bidder.eps))
-        print ("Arm Chosen")
-        print arm_chosen
+        for b in range(0, bidder.bid_space):
+            u_s = 0
+            for auction in range(0,num_auctions):
+                u_s += bidder.reward_func[auction][t][b]*bidder.alloc_func[auction][t][b]
+            bidder.avg_reward[t].append(u_s/num_auctions)
+            bidder.avg_utility[t].append(bidder.avg_reward[t][b] - 1)
+        arm_chosen = int(math.ceil(bids[0][t][bidder.id]/bidder.eps))
+        #print ("Arm Chosen")
+        #print arm_chosen
+        #print ("Average reward at timestep t=%d is:"%t)
+        #print (bidder.avg_reward[t])
+        #print ("Average utility at timestep t=%d is:"%t)
+        #print (bidder.avg_utility[t])
 
-        print ("Probability vector before computing estimated loss")
-        print bidder.pi
-       
-        estimated_loss = bidder.utility[t][arm_chosen]/bidder.pi[arm_chosen]
-        print ("Estimated Loss")
-        print estimated_loss
+        #print ("Probability vector before computing estimated loss")
+        #print bidder.pi
+        
+        if bidder.pi[arm_chosen] < 0.0000000001:
+            bidder.pi[arm_chosen] = 0.0000000001
+        estimated_loss = bidder.avg_utility[t][arm_chosen]/bidder.pi[arm_chosen]
+        #print ("Estimated Loss")
+        #print estimated_loss
         bidder.loss[arm_chosen] += estimated_loss
-        print bidder.loss
+        #print bidder.loss
         bidder.weights = [math.exp(bidder.eta_exp3*bidder.loss[b]) for b in range(0,bidder.bid_space)]
         bidder.pi = [bidder.weights[b]/sum(bidder.weights) for b in range(0,bidder.bid_space)]
         
-        print ("Probability vector after computing estimated loss")
-        print bidder.pi
-        #for i in range(0,num_bidders):
-        #    #every bidder is a learner and they all should update the estimated utility that they get
-        #    allocated = GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder[i].id, bids[t][bidder[i].id])
-        #    bidder[i].alloc_func[t] = [GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder[i].id, bid*bidder[i].eps) for bid in range(0, bidder[i].bid_space)]
-        #    #reward function: value - payment(coming from GSP module)
-        #    bid_vec = deepcopy(bids[t])
-        #    bidder[i].pay_func[t] = [GSP(ctr[t], reserve[t], bid_vec, rank_scores[t], num_slots, num_bidders).pay_func(bidder[i].id, bid*bidder[i].eps) for bid in range(0, bidder[i].bid_space)]  
-    
-        #    bidder[i].reward_func[t] = compute_reward(bidder[i].alloc_func[t], bidder[i].pay_func[t], ctr[t], values[t])
-        #    #### EXP3 computations ####
-        #    bidder[i].utility[t] = [bidder[i].reward_func[t][b]*bidder[i].alloc_func[t][b] - 1 for b in range(0, bidder[i].bid_space)]
-        #    print ("Bidder %d utility"%bidder[i].id)
-        #    print bidder[i].utility[t]
+        #print ("Probability vector after computing estimated loss")
+        #print bidder.pi
+        # compute the algorithm's utility at every step
+        algo_util.append(bidder.avg_utility[t][int(math.ceil(bids[0][t][bidder.id]/bidder.eps))])
+        temp_regr.append(regret(0,bidder.reward_func,bidder.alloc_func,bidder.bid_space, algo_util,t,num_auctions))    
+        #print ("Regret inside )
+        #print temp_regr
 
-        #    #weights update
-        #    print bids[t], bids[t][bidder[i].id]
-        #    arm_chosen = int(math.ceil(bids[t][bidder[i].id]/bidder[i].eps))
-        #    print ("Arm Chosen")
-        #    print arm_chosen
-
-        #    print ("Probability vector before computing estimated loss")
-        #    print bidder[i].pi
-        #   
-        #    estimated_loss = bidder[i].utility[t][arm_chosen]/bidder[i].pi[arm_chosen]
-        #    print ("Estimated Loss")
-        #    print estimated_loss
-        #    bidder[i].loss[arm_chosen] += estimated_loss
-        #    print bidder[i].loss
-        #    bidder[i].weights = [math.exp(bidder[i].eta_exp3*bidder[i].loss[b]) for b in range(0,bidder[i].bid_space)]
-        #    bidder[i].pi = [bidder[i].weights[b]/sum(bidder[i].weights) for b in range(0,bidder[i].bid_space)]
-            
-        #    print ("Probability vector after computing estimated loss")
-        #    print bidder[i].pi
-        
-    # after the end of T timesteps, compute regrets for both the EXP3 algo
-    #for i in range(0, num_bidders):
-    #    algo_util = [bidder[i].utility[t][int(math.ceil(bids[t][bidder[i].id]/bidder[i].eps))] for t in range(0,T)]
-    #    bidder[i].exp3_regret[curr_rep] = regret(0, bidder[i].reward_func, bidder[i].alloc_func, bidder[i].bid_space, algo_util, T)
-    
-    algo_util = [bidder.utility[t][int(math.ceil(bids[t][bidder.id]/bidder.eps))] for t in range(0,T)]
-    bidder.exp3_regret[curr_rep] = regret(0, bidder.reward_func, bidder.alloc_func, bidder.bid_space, algo_util, T)
-    #s = 0
-    #for i in range(0, num_bidders):
-    #    s += bidder[i].exp3_regret[curr_rep]
-    
-    #return s/num_bidders
-    return bidder.exp3_regret[curr_rep]   
+    return temp_regr   
 
 
--- runner_winexp_all_bidders.py
+++ runner_winexp_all_bidders.py
@@ -14,7 +14,9 @@ from regret import *
 
 # if he gets allocated a slot, then reward = value - payment
 # else reward = 0
-def compute_reward(allocation_func, payment_func, ctr, values):
+def compute_reward(allocation_func, payment_func, ctr, values,num_auctions):
+    algo_util  = []
+    temp_regr  = []
     reward_lst = []
     for b in allocation_func:
         if b in ctr:
@@ -26,90 +28,55 @@ def compute_reward(allocation_func, payment_func, ctr, values):
     return reward_lst
 
 
-def main_winexp(bidder,curr_rep, T,num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values,bids):
-    #bids = []
+def main_winexp(bidder,curr_rep, T,num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values,bids, num_auctions):
+    algo_util = []
+    temp_regr = []
     for t in range(0,T):
-        #bids.append([round(bidder[i].bidding(),2) for i in range(0,num_bidders)])
-        bids[t][0] = round(bidder.bidding(),2)
-        #print("Bids inside winexp at timestep %d"%t)
-        #print bids[t]
-        #every bidder is a learner and they all should update the estimated utility that they get
-        allocated = GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder.id, bids[t][bidder.id])
-        bidder.alloc_func[t] = [GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]
+        bid_chosen = round(bidder.bidding(),2)
+        for auction in range(0,num_auctions):
+            # same bid over all auctions that run at the same timestep
+            bids[auction][t][0] = bid_chosen
+            #every bidder is a learner and they all should update the estimated utility that they get
+            allocated = GSP(ctr[auction][t], reserve[auction][t], bids[auction][t], rank_scores[auction][t], num_slots, num_bidders).alloc_func(bidder.id, bids[auction][t][bidder.id])
+            bidder.alloc_func[auction][t] = [GSP(ctr[auction][t], reserve[auction][t], bids[auction][t], rank_scores[auction][t], num_slots, num_bidders).alloc_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]
         #reward function: value - payment(coming from GSP module)
-        bid_vec = deepcopy(bids[t])
-        bidder.pay_func[t] = [GSP(ctr[t], reserve[t], bid_vec, rank_scores[t], num_slots, num_bidders).pay_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]  
-        bidder.reward_func[t] = compute_reward(bidder.alloc_func[t], bidder.pay_func[t], ctr[t], values[t])
-        #print ("Bidder's Reward Function")
-        #print bidder.reward_func[t]
-        #### WIN-EXP computations ####
-        if allocated != 0: #only if he gets allocated originally he can get information     
-            #updates the bidder's estimate of the utility
-            bidder.utility[t] = (bidder.compute_utility(1, bidder.reward_func[t], bidder.alloc_func[t]))
-        else:
-            bidder.utility[t] = (bidder.compute_utility(0, bidder.reward_func[t], bidder.alloc_func[t]))
-        bidder.utility[t] = [0 if bidder.utility[t][bb] == -0.0 else bidder.utility[t][bb] for bb in range(0,bidder.bid_space)]
-        #weights update
-        #print ("Bidder %d allocation function"%bidder.id)
-        #print bidder.alloc_func[t]
-        #print ("Bidder %d payment function"%bidder.id)
-        #print bidder.pay_func[t]
-        #print ("Bidder %d utility"%bidder.id)
-        #print bidder.utility[t]
-        #print ("Bidder %d weights and probabilities before the update at timestep %d"%(bidder.id,t))
-        #print bidder.weights
-        #print bidder.pi 
-        #bidder.weights_update_winexp(bidder.eta_winexp, bidder.utility[t])        
-        #print ("Bidder %d weights and probabilities after the update at timestep %d"%(bidder.id,t))
-        #print bidder.weights
-        #print bidder.pi 
-        #for i in range(0,num_bidders):
-        #    #every bidder is a learner and they all should update the estimated utility that they get
-        #    allocated = GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder[i].id, bids[t][bidder[i].id])
-        #    bidder[i].alloc_func[t] = [GSP(ctr[t], reserve[t], bids[t], rank_scores[t], num_slots, num_bidders).alloc_func(bidder[i].id, bid*bidder[i].eps) for bid in range(0, bidder[i].bid_space)]
-        #    #reward function: value - payment(coming from GSP module)
-        #    bid_vec = deepcopy(bids[t])
-        #    bidder[i].pay_func[t] = [GSP(ctr[t], reserve[t], bid_vec, rank_scores[t], num_slots, num_bidders).pay_func(bidder[i].id, bid*bidder[i].eps) for bid in range(0, bidder[i].bid_space)]  
-        #    bidder[i].reward_func[t] = compute_reward(bidder[i].alloc_func[t], bidder[i].pay_func[t], ctr[t], values[t])
-        #    #### WIN-EXP computations ####
-        #    if allocated != 0: #only if he gets allocated originally he can get information     
-        #        #updates the bidder's estimate of the utility
-        #        bidder[i].utility[t] = (bidder[i].compute_utility(1, bidder[i].reward_func[t], bidder[i].alloc_func[t]))
-        #    else:
-        #        bidder[i].utility[t] = (bidder[i].compute_utility(0, bidder[i].reward_func[t], bidder[i].alloc_func[t]))
-        #    bidder[i].utility[t] = [0 if bidder[i].utility[t][bb] == -0.0 else bidder[i].utility[t][bb] for bb in range(0,bidder[i].bid_space)]
-        #    #weights update
-        #    print ("Bidder %d utility"%i)
-        #    print bidder[i].utility[t]
-        #    print ("Bidder %d weights and probabilities before the update at timestep %d"%(i,t))
-        #    print bidder[i].weights
-        #    print bidder[i].pi 
-        #    bidder[i].weights_update_winexp(bidder[i].eta_winexp, bidder[i].utility[t])        
-        #    print ("Bidder %d weights and probabilities after the update at timestep %d"%(i,t))
-        #    print bidder[i].weights
-        #    print bidder[i].pi 
-        
-        
-    # after the end of T timesteps, compute regrets for both the WIN-EXP algo
-    #for i in range(0, num_bidders):
-    #    arm_chosen = [int(math.ceil(bids[t][bidder[i].id]/bidder[i].eps)) for t in range(0,T)]  
-    #    algo_util = [(bidder[i].reward_func[t][arm_chosen[t]]*bidder[i].alloc_func[t][arm_chosen[t]] - 1) for t in range(0,T)]
-    #    bidder[i].winexp_regret[curr_rep] = regret(0, bidder[i].reward_func, bidder[i].alloc_func, bidder[i].bid_space, algo_util, T)
-    #print ("Bid:")
-    #print ([bids[t][0] for t in range(0,T)])
-    #print ("Arms Chosen:") 
-    #arm_chosen = [int(math.ceil(bids[t][bidder.id]/bidder.eps)) for t in range(0,T)]  
-    #print arm_chosen
-    algo_util = [(bidder.reward_func[t][arm_chosen[t]]*bidder.alloc_func[t][arm_chosen[t]] - 1) for t in range(0,T)]
-    #print ("Rep %d"%curr_rep)
-    #print ("algo's utility:")
-    #print algo_util
-    bidder.winexp_regret[curr_rep] = regret(0, bidder.reward_func, bidder.alloc_func, bidder.bid_space, algo_util, T)
+            bid_vec = deepcopy(bids[auction][t])
+            bidder.pay_func[t] = [GSP(ctr[auction][t], reserve[auction][t], bid_vec, rank_scores[auction][t], num_slots, num_bidders).pay_func(bidder.id, bid*bidder.eps) for bid in range(0, bidder.bid_space)]  
+            bidder.reward_func[auction][t] = compute_reward(bidder.alloc_func[auction][t], bidder.pay_func[t], ctr[auction][t], values[auction][t],num_auctions)
+            #print ("Bidder's Reward Function")
+            #print bidder.reward_func[t]
+            #### WIN-EXP computations ####
+            if allocated != 0: #only if he gets allocated originally he can get information     
+                #updates the bidder's estimate of the utility
+                bidder.utility[auction][t] = (bidder.compute_utility(1, bidder.reward_func[auction][t], bidder.alloc_func[auction][t]))
+            else:
+                bidder.utility[auction][t] = (bidder.compute_utility(0, bidder.reward_func[auction][t], bidder.alloc_func[auction][t]))
+            #TODO check what the probability of the outcome should be in this case
+            #bidder.utility[t] = [0 if bidder.utility[t][bb] == -0.0 else bidder.utility[t][bb] for bb in range(0,bidder.bid_space)]
 
-    #s = 0
-    #for i in range(0, num_bidders):
-    #    s += bidder[i].winexp_regret[curr_rep]
-    #return s/num_bidders
-    return bidder.winexp_regret[curr_rep]   
+
+        #TODO check again
+        for b in range(0, bidder.bid_space):
+            u_s = 0
+            for auction in range(0,num_auctions):
+                u_s += bidder.reward_func[auction][t][b]*bidder.alloc_func[auction][t][b] 
+            #bidder.avg_utility[t].append(bidder.avg_reward[t][b]-1)
+            bidder.avg_reward[t].append(u_s/num_auctions)
+            bidder.avg_utility[t].append(bidder.avg_reward[t][b]-1)
+
+        (bidder.weights, bidder.pi) = bidder.weights_update_winexp(bidder.eta_winexp, bidder.avg_utility[t])        
+        # for each auction (at the same t) you choose the same arm
+        arm_chosen = int(math.ceil(bids[0][t][bidder.id]/bidder.eps))   
+
+        #TODO turn this into averages
+
+        algo_util_auction = []
+        for auction in range(0, num_auctions):
+            algo_util_auction.append((bidder.reward_func[auction][t][arm_chosen]*bidder.alloc_func[auction][t][arm_chosen]))
+
+        algo_util.append(np.mean(algo_util_auction)-1)
+        temp_regr.append(regret(0, bidder.reward_func, bidder.alloc_func, bidder.bid_space, algo_util, t, num_auctions))
+
+    return temp_regr   
 
 
