f76a187c4edff10f3de0673a1e7708f498ae4b03
charapod <podimata@g.harvard.edu>
2018-05-11 00:20:10 -0400

    updated winexp_adversaries_05

---
+++ different_ctr/winexp_adversaries_05/auction_parameters.py
@@ -0,0 +1,36 @@
+########
+# File that specifies the auction parameters to be used in each iteration. The same parameters are used subsequently by each repetition.
+########
+
+
+import numpy as np
+import random
+import math
+
+def set_auction_params(T,num_repetitions):
+    num_bidders         = 20
+    num_slots           = 3
+    outcome_space       = 2
+    #Create the rank scores. Size of rank_scores: T x num_bidders
+    rank_scores = [[np.random.uniform(0,1) for i in range(0,num_bidders)] for _ in range(0,T)]  
+    # at every t, the rank scores are the same, irrespective of the auction that we're currently participating in
+    #size of CTR: T x num_slots
+    ctr  = [[np.random.uniform(0.5,1) for _ in range(0,num_slots)] for _ in range(0,T)] 
+    for t in range(0, T):
+        ctr[t].sort(reverse=True)
+    
+    #size of reserve: num_auctions x T
+    reserve = [np.random.uniform(0,0.3) for i in range(0,T)]   
+    # value function for every slot
+    # could be seen as the conversion rate for every slot
+    # size of values:  T x num_bidders 
+    values = [[np.random.uniform(0,1) for j in range(0,num_bidders)] for _ in range(0,T)]
+    # threshold ctr to decide whether or you get clicked or not
+    threshold = [np.random.uniform(0,1) for _ in range(0,T)]
+
+    # gaussian noise on all timesteps on all slots
+    # we assume that the last slot + 1 corresponds to the "no-allocation" case
+    #noise = [np.random.normal(0,math.sqrt(1.0/100),num_slots+1) for _ in range(0,T)]
+    noise = [[0 for _ in range(0,num_slots+1)] for _ in range(0,T)]
+    
+    return (num_bidders, num_slots, outcome_space,rank_scores, ctr, reserve, values, threshold,noise)
---
+++ different_ctr/winexp_adversaries_05/master_file.py
@@ -0,0 +1,77 @@
+import numpy as np
+import random
+import math
+from copy import deepcopy
+from runner_winexp_all_bidders import *
+from runner_exp3_all_bidders import *
+from runner_gexp3_all_bidders import *
+from multiprocessing import Pool
+
+winexp_regr = []
+exp3_regr   = []
+
+
+def log_winexp_results(res):
+    winexp_regr.append(res)
+
+def log_exp3_results(res):
+    exp3_regr.append(res)
+
+def regret_winexp(bidder, T,num_repetitions, num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values, bids,threshold,noise,num_adaptive):
+    f1 = "winexp_regrets.txt"
+    winexp_regrets = open(f1, "w")
+    #winexp_regr is now a num_repetitionsxT matrix
+
+    pool = Pool(processes = num_repetitions)
+    results = [pool.apply_async(main_winexp, args = (bidder[rep],rep, T, num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values,bids, threshold,noise,num_adaptive), callback = log_winexp_results) for rep in range(0,num_repetitions)]
+    
+
+    pool.close()
+    pool.join()   
+
+    winexp_expected_regr = []
+    for t in range(0,T):
+        winexp_expected_regr.append(sum(d[t] for d in winexp_regr)/num_repetitions)
+
+    for r in range(0,num_repetitions):
+        s = ""
+        for t in range(0,T):
+            s += ("%.5f "%winexp_regr[r][t])
+        s += "\n"
+        winexp_regrets.write(s) 
+    
+
+    final_winexp_regr = winexp_expected_regr
+    winexp_regrets.close()
+    return (final_winexp_regr, winexp_regr)
+
+
+def regret_exp3(bidder, T,num_repetitions, num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values,bids,threshold, noise,num_adaptive):
+    f2 = "exp3_regrets.txt"
+    exp3_regrets = open(f2,"w")
+    #exp3_regr is now a num_repetitionsxT matrix
+    pool = Pool(processes=num_repetitions)
+    results = [pool.apply_async(main_exp3, args = (bidder[rep],rep, T, num_bidders, num_slots, outcome_space, rank_scores, ctr, reserve, values,bids, threshold,noise,num_adaptive), callback = log_exp3_results) for rep in range(0,num_repetitions)]
+    
+
+    pool.close()
+    pool.join()   
+ 
+
+    for r in range(0,num_repetitions):
+        s = ""
+        for t in range(0,T):
+            s += ("%.5f "%exp3_regr[r][t])
+        s += "\n"
+        exp3_regrets.write(s) 
+        
+    # below, we compute the cumulative regret for the number of repetitions we ran rounds 0->T
+    exp3_expected_regr = []
+    for t in range(0,T):
+        exp3_expected_regr.append(sum(d[t] for d in exp3_regr)/num_repetitions)
+
+    final_exp3_regr = exp3_expected_regr
+    exp3_regrets.close()
+    return (final_exp3_regr, exp3_regr)
+
+
