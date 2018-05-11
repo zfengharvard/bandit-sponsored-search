e85f5f7c31c4654f63c73e73900d5134252640bd
charapod <podimata@g.harvard.edu>
2018-05-10 22:54:56 -0400

    changes in plots.R

--- different_ctr/oblivious_01/plots.R
+++ different_ctr/oblivious_01/plots.R
@@ -28,50 +28,11 @@
 
     probs  <- c(0.1, 0.9) # the percentiles to be used
   
-    #df_aggr$y_low = rowQuantiles(df_aggr, rows = NULL, cols = NULL, probs = c(0.1), na.rm = FALSE, type = 7L, drop = TRUE)
-    #mutate(df_aggr, avg_regr = rowMeans(df_aggr))
-    #mutate(df_aggr, y_low = rowQuantiles(df_aggr, rows = NULL, cols = NULL, probs = c(0.1), na.rm = FALSE, type = 7L, drop = TRUE))
-    #mutate(df_aggr, y_high = rowQuantiles(df_aggr, rows = NULL, cols = NULL, probs = c(0.9), na.rm = FALSE, type = 7L, drop = TRUE))
-    
-           
-    
-    #win_mat     <- matrix(winexp, nrow=30, ncol=10000)
-    #print (winexp)
-    #print (win_mat)
-     
-    #input_file = 'data_sampled.csv'
-    #input_file = 'photo_errors_output_ts05_0_5_2.csv'
-    #input_file = 'event_errors_output_ts05_0_5_2.csv'
-    #df <- read.csv(input_file)
-    #df <- df %>% group_by(c_s, method) %>% 
-    #  summarize(avg_error = mean(value), sd = sd(value)) %>% 
-    #  mutate(y_low = avg_error - sd) %>% 
-    #  mutate(y_high = avg_error + sd) %>% 
-    #  mutate(method_name = method) %>%
-    #  mutate(
-    #    method = ifelse(method=='lr', 'Hyperparametric', 
-    #                    ifelse(method=='lr_augmented', 'Hyp. augmented', 
-    #                           ifelse(method=='lr_reduced', 'Hyp. reduced', 
-    #                                  ifelse(method=='mle_full', 'MLE omniscient',
-    #                                  		ifelse(method=='mle_partial', 'Non-hyperparam.', '-')
-    #                                  )
-    #                          )
-    #                    )
-    #            )
-    #    ) %>%
-    #  mutate(Method = method) %>%
-    #  ungroup()
     
 
     ggplot(df_aggr) +
       geom_ribbon(aes(x = rounds, ymin = y_low, ymax = y_high, fill=Algorithm), alpha=0.3) + guides(fill=FALSE) +
-      #geom_errorbar(aes(x = c_s, ymin = y_low, ymax = y_high), width=0.05, color = 'black', size = 0.1) +
       geom_line(aes(x=rounds, y=avg_regr, color = Algorithm, linetype = Algorithm), size = 1) +
-    #  geom_point(aes(x=c_s, y=avg_error, color = Method, shape=Method), size = 3) +
-    #  scale_x_log10(
-    #      breaks = c(1e0, 1e2, 1e3, 1e4, 1e5),
-    #      labels = c(1e0, 1e2, 1e3, 1e4, 1e5)
-    #  ) +
       scale_y_continuous(
         breaks = c(0,10,20,30,40,50,60)
       )+
